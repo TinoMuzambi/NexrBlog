92ec356ea270c56b2c37ef7bf7f30a79d0d9f026
Tino <tinomuzambi@gmail.com>
2021-01-31 13:01:44 +0200

    Adds appending server to url.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import { server } from "../config";
+
 export default function Home() {
 	return "";
 }
 
-export const getStaticProps = async () => {};
+export const getStaticProps = async () => {
+	const res = await fetch(`${server}/api/blogs`);
+	const blogs = await res.json();
+
+	return {
+		props: {
+			blogs,
+		},
+	};
+};
