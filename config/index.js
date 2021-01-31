a4a5a8acada91b9258cbd427e1300e42637456dc
Tino <tinomuzambi@gmail.com>
2021-01-31 20:03:27 +0200

    Changing production url won't work.

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
 	? "http://localhost:3000"
-	: "https://next-blog-swart-eight.vercel.app";
+	: "https://blog.tinomuzambi.com";
