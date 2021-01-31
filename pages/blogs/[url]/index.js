b335f52dfc801bd865c991a70c80a592f5aeb810
Tino <tinomuzambi@gmail.com>
2021-01-31 15:33:49 +0200

    Displaying rudimentary blog details working.

--- pages/blogs/[url]/index.js
+++ pages/blogs/[url]/index.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { server } from "../../../config";
 
-const blog = () => {
-	return <div>Blog</div>;
+const blog = ({ blog }) => {
+	return (
+		<div>
+			<h1>{blog.title}</h1>
+		</div>
+	);
 };
 
 export default blog;
+
+export const getStaticProps = async (context) => {
+	const res = await fetch(`${server}/api/blogs/${context.params.url}`);
+
+	const blog = await res.json();
+
+	return {
+		props: {
+			blog,
+		},
+	};
+};
+
+export const getStaticPaths = async () => {
+	const res = await fetch(`${server}/api/blogs`);
+
+	const blogs = await res.json();
+
+	const urls = blogs.map((blog) => blog.url);
+	const paths = urls.map((url) => ({ params: { url: url.toString() } }));
+
+	return {
+		paths,
+		fallback: false,
+	};
+};
