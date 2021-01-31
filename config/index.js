084fe660d8263a60cf8cf214337ca5804a1cb9dd
Tino <tinomuzambi@gmail.com>
2021-01-31 20:01:24 +0200

    Changes production url.

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
 	? "http://localhost:3000"
-	: "https://blog.tinomuzambi.com";
+	: "https://next-blog-swart-eight.vercel.app";
