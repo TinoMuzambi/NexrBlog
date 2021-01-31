a3fa223662e3ea11fdadce95054f52209e4a26c9
Tino <tinomuzambi@gmail.com>
2021-02-01 00:23:32 +0200

    Adds actions to reducer.

--- context/AppReducer.js
+++ context/AppReducer.js
@@ -1,5 +1,20 @@
 export default (state, action) => {
 	switch (action.type) {
+		case "UPDATE_BLOGS":
+			return {
+				...state,
+				blogs: action.payload,
+			};
+		case "UPDATE_CATEGORIES":
+			return {
+				...state,
+				categories: action.payload,
+			};
+		case "UPDATE_ITEM":
+			return {
+				...state,
+				item: action.payload,
+			};
 		default:
 			return state;
 	}
