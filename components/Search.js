c93171ffabd416c42373c467205051eff9084cb7
Tino <tinomuzambi@gmail.com>
2021-02-18 11:55:25 +0200

    Some more dependencies and components.

---
+++ components/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Search = ({ query, searchBlogs }) => (
+	<div className="search-field">
+		<input
+			type="text"
+			id="searchBlogs"
+			placeholder="Search Blogs"
+			className="search-input"
+			value={query}
+			onChange={(e) => searchBlogs(e.target.value)}
+		/>
+	</div>
+);
+
+export default Search;
