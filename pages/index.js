e05b526d37894adbe27829a2bc1ac689c8e896f5
Tino <tinomuzambi@gmail.com>
2021-02-22 13:57:09 +0200

    Search working with default url.

--- pages/index.js
+++ pages/index.js
@@ -41,6 +41,7 @@ function Home({ blogs, categories, featuredItem, router }) {
 	useEffect(() => {
 		const fromOpenSearch = router.query?.fromOpenSearch;
 		if (fromOpenSearch) {
+			console.log(router.query?.queryText);
 			setQueryText(router.query?.queryText);
 			executeScroll(blogsRef);
 		}
--- pages/search/[query].js
+++ pages/search/[query].js
@@ -1,22 +1,48 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
-const OpenSearch = () => {
+import Preload from "../../components/Preload";
+
+const OpenSearch = ({ query }) => {
 	const router = useRouter();
 
+	if (router.isFallback) {
+		return <Preload />;
+	}
+
 	useEffect(() => {
-		const queryText = router.query.query;
-		console.log(queryText);
+		console.log(query);
 		router.push({
 			pathname: "/",
 			query: {
 				fromOpenSearch: true,
-				queryText: queryText,
+				queryText: query.query,
 			},
 		});
 	}, [router.pathname]);
 
 	return null;
+};
+
+export const getStaticProps = async ({ params }) => {
+	console.log(params.url);
+	const query = {
+		query: params.url || "hitfilm",
+	};
+	console.log(query);
+
+	return {
+		props: {
+			query,
+		},
+	};
+};
+
+export const getStaticPaths = () => {
+	return {
+		paths: [],
+		fallback: true,
+	};
 };
 
 export default OpenSearch;
